3e676c54388921f97d345f8cdb9994ad0ac29a68
Shijit Dasgupta <shijitdasgupta@Shijits-MacBook-Pro.local>
2017-12-24 01:02:03 -0500

    updates

--- Hangman.py
+++ Hangman.py
@@ -1,20 +1,19 @@
 import random
-words = ['chicken','turkey','pork','tofu','beef']
-word = words[random.randint(0,4)]
+words = ['chicken', 'turkey', 'pork', 'tofu',' beef']
+word = words[random.randint(0, 4)]
 print(word)
 letters_guessed = []
 correct_letters = []
-letter = input('Please input a letter to check')
-letters_guessed.append(letter)
-letters_set = set(letters_guessed)
+
 word_not_guessed = True
 while word_not_guessed is True:
-
+    letter = input('Please input a letter to check: ')
+    letters_guessed.append(letter)
+    letters_set = set(letters_guessed)
     for word in words:
         if letters_set & set(word):
             print('Yes! that letter exists!')
             correct_letters.append(letters_set)
-            letter = input('Please input a letter to check')
 
         elif len(word) == len(letters_guessed):
             print('Yes! that letter exists!')
