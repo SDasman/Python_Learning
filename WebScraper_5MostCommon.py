3a72c673b1d05fc6f88c63b63cdad629fb59b8e4
Shijit Dasgupta <shijitdasgupta@Shijits-MacBook-Pro.local>
2017-12-24 00:50:55 -0500

    Hangman Attempt 1

--- WebScraper_5MostCommon.py
+++ WebScraper_5MostCommon.py
@@ -3,10 +3,11 @@ from collections import Counter
 import requests
 from string import punctuation
 
-r = requests.get("http://en.wikipedia.org/wiki/Wolfgang_Amadeus_Mozart")
+r = requests.get("http://www.literaturepage.com/read/prideandprejudice-1.html")
 
 soup = BeautifulSoup(r.content, "html5lib")
 
 text = (''.join(s.findAll(text=True))for s in soup.findAll('p'))
 c = Counter((x.rstrip(punctuation).lower() for y in text for x in y.split()))
-print (c.most_common(5)) # prints most common words staring at most common.
+print (c.most_common(5))
+# This prints most common words staring at most common.
