cecd33ad162bc086b19b68fd900e18593ee52491
Shijit Dasgupta <shijitdasgupta@Shijits-MacBook-Pro.local>
2018-01-09 19:31:09 -0500

    Calculator update

--- WebScraper_5MostCommon.py
+++ WebScraper_5MostCommon.py
@@ -2,7 +2,7 @@ from bs4 import BeautifulSoup
 from collections import Counter
 import requests
 from string import whitespace, punctuation
-r = requests.get("http://www.literaturepage.com/read/prideandprejudice-1.html")
+r = requests.get("http://www.literaturepage.com/read/pollyanna-1.html")
 
 soup = BeautifulSoup(r.content, "html5lib")
 
@@ -11,7 +11,7 @@ text = (word.lower().strip(whitespace + punctuation)
         for text in element.findAll(text=True)
         for word in text.split())
 
-words_to_exclude = {'a', 'or', 'the', 'of', 'it', 'is', 'it', 'and', 'in', 'she', 'he', 'to'}
+words_to_exclude = {'a', 'or', 'the', 'of', 'it', 'is', 'it', 'and', 'in', 'she', 'he', 'to', 'i'}
 
 important = Counter(word for word in text if word not in words_to_exclude)
 
